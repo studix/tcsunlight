336b8b3452f0407816bfe6f599447394f5d07855
studix <Marco@iMac.local>
2014-06-25 12:02:22 +0200

    database password

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,15 +1,16 @@
 require 'bundler/capistrano'
+#require '/Users/Marco/Development/tcsunlight/credentials/tcsunlight_app.rb'
 set :scm, :git
+set :user, "studer"
 set :repository, "git@github.com:studix/tcsunlight.git" 
 set :branch, "master"
-
-require '../credentials/sample_app.rb'
+set :ssh_options, { :forward_agent => true }
 
 set :server_name, 'studix.railsplayground.net'
 set :application, 'tcsunlight'
 set :normalize_asset_timestamps, false 
 
-
+set :deploy_to, "/home/#{user}/#{application}"
 set :use_sudo, false
 set :group_writable, false
 default_run_options[:pty] = true
