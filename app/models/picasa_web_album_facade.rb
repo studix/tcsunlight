27c7c7c6889065cf4b3d3eb6ed1b4ddc275579e5
studix <marco.studer@gmx.net>
2016-08-24 17:59:48 +0200

    Picasa Web Migration 2

--- app/models/picasa_web_album_facade.rb
+++ app/models/picasa_web_album_facade.rb
@@ -3,7 +3,25 @@ class PicasaWebAlbumFacade
   require 'net/https'
   @@picasa_url = 'http://picasaweb.google.com/data/feed/api/user/113115968878931614254'
 
-  @google_photos_url_mapping = { "6322092917200132817" => "https://photos.google.com/share/AF1QipPz-rpL2jqdkyc7B__w6TgZ-OmUqOapMy1D2cmzpZXsr0Al8Af3nFhBdR5MBQEhUA?key=anpibjk4a1dON2pMcEw0bWxPSGJscl93eTljQjV3" }
+  @google_photos_url_mapping = { "6322092917200132817" => "https://goo.gl/photos/Y1iP4q1McMjacf956", #CM 2016
+                                 "6188873569815108129" => "https://goo.gl/photos/F9tM6tofHFx7SVSA6", #CM 2015
+                                 "6069220280649896577" => "https://goo.gl/photos/X9WM9tARn4zuzeKVA", #CM 2014
+                                 "6027673502619115985" => "https://goo.gl/photos/RNKm698XyM23drUu8", #Hopp Schwiiz 2014
+                                 "5916304782174952801" => "https://goo.gl/photos/CSCEdo9M1JCM1fjK8", #CM 2013
+                                 "5785788968661934049" => "https://goo.gl/photos/X57vUjYaJCtBCbb67", #CM 2012
+                                 "5646321407522660097" => "https://goo.gl/photos/PLsTja8Jj4XV3Wo6A", #CM 2011
+                                 "5489546439564447921" => "https://goo.gl/photos/o84MoZ2QHzdSBcPo6", #Hopp Schwiiz 2010
+                                 "5375438016275558593" => "https://goo.gl/photos/qnsoLAybqHZMd6hC7", #CM 2009
+                                 "5239650248778752545" => "https://goo.gl/photos/UfQB4PyhHtMXAB2E7", #CM 2008
+                                 "5212048226829497361" => "https://goo.gl/photos/7r8BpyamJatn3oBE6", #Hopp Schwiiz 2008
+                                 "5192731291146577057" => "https://goo.gl/photos/4yGp5NKo4gT1e7tbA", #IC Ligaerhalt 2005
+                                 "5192730784340435937" => "https://goo.gl/photos/5ZtR7HCGYcwL63Vq5", #IC Ligaerhalt 2001
+                                 "5192748458130860321" => "https://goo.gl/photos/to5qijmyDKN5wrgN9", #CM 2006
+                                 "5192746589820086481" => "https://goo.gl/photos/sDuqoYWfyeb8dDiPA", #CM 2005,
+                                 "5192742230428280737" => "https://goo.gl/photos/J66ULJvPGG5Sz1w96", #CM 2004,
+                                 "5192749592002226609" => "https://goo.gl/photos/gAshAiivvXUusQs96", #IC 2004,
+                                 "5192737991295559345" => "https://goo.gl/photos/x4wxFfxGuFK78NN36", #CM 2003,
+                                 "5192733022018397633" => "https://goo.gl/photos/Hv3tvFhqdJN2ZmVu9"} #Nachtturnier 2002,
   
   def self.get_album_feed
     uri = URI.parse(@@picasa_url)
